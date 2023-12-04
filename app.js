6610e418061983dee48d09d8d4e3c2969927bdfc
LampkinSynk <cejajaylin5@gmail.com>
2023-12-03 21:47:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//creating variables to utilze node node packages
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+// customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title', 
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse();
+
